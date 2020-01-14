b0f57b866da01e0413787d95964f47d7c3d8e8b8
Tomas Vosylius <vsls.tomas@gmail.com>
2020-01-14 10:42:00 +0100

    Add poll.py

---
+++ poll.py
@@ -0,0 +1,63 @@
+# poll.py 
+
+# functions
+def command_input():
+    text = "Enter selected command: show polls, create poll, quit: "
+    user_in = input(text)
+    return user_in
+
+# procedure
+polls = [
+    {
+        'question':'Example poll',
+        'answers': [
+            'Answer 1',
+            'Answer 2'
+        ]
+    }
+]
+user_in = command_input()
+
+while user_in != 'quit':
+    if user_in == 'create poll':
+        # create new poll 
+        question = input("# Creating new poll\nEnter new poll question:\n") 
+
+        new_poll = {
+            'question': question,
+            'answers': []
+        }
+
+        while(True):
+            possible_answer = input("# Add available answer no. " 
+                                    + str(len(new_poll['answers']) + 1) 
+                                    + " \n( enter 'stop' to quit creating answers ):")
+            if possible_answer == 'stop':
+                break
+            # append the answer
+            new_poll['answers'].append(possible_answer)
+        # append the poll
+        polls.append(new_poll)
+
+    elif user_in == 'show polls':
+        p = 1
+        for poll in polls:
+            print(str(p) + " poll: " + poll['question'])
+            print("Available (" + str(len(poll['answers'])) + ") answers:")
+            
+            # show answers
+            i = 1
+            for ans in poll['answers']:
+                print("\t" + str(i) + ". " + str(ans))
+                i += 1;
+
+            p += 1
+
+    else:
+        # command unknown
+        print("Command unknown!")
+    
+    # show the input again
+    user_in = command_input()
+
+print("Have a nice day!")
